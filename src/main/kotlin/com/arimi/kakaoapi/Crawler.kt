701912ccd502407be43a29f34ef8309415ec2a50
makehoney <makehoney3@gmail.com>
2019-05-07 16:01:11 +0900

    [Add feature] 기숙사 식당 시단 단위 크롤링 기능 추가

--- src/main/kotlin/com/arimi/kakaoapi/Crawler.kt
+++ src/main/kotlin/com/arimi/kakaoapi/Crawler.kt
@@ -14,14 +14,13 @@ class Crawler {
 
     object FoodCourtOf {
         private var ajouTables: List<WebElement>
-        private var foodCourts: HashMap<String, WebElement?>
-
+        private var foodCourtsHtml: HashMap<String, WebElement?>
         init {
             driver.get("http://www.ajou.ac.kr/main/life/food.jsp")
 
             // driver.get 이후 실행되어야 하므로 init 블록 사용
             ajouTables = driver.findElements(By.className("ajou_table"))
-            foodCourts = hashMapOf (
+            foodCourtsHtml = hashMapOf (
                     "studentFood" to ajouTables[0],
                     "dormFood" to ajouTables[1],
                     "facultyFood" to ajouTables[2]
@@ -29,11 +28,47 @@ class Crawler {
         }
 
         fun student(): String? {
-            return foodCourts["studentFood"]?.text
+//            foodCourtsHtml["studentFood"]?.findElement
+            return ""
+        }
+
+        fun dorm() {
+            val dormFoodMap = HashMap<String, String>()
+            foodCourtsHtml["dormFood"]?.findElements(By.tagName("tr"))
+                    ?.map {
+                        var timeBuf = ""
+
+                        // tr 자식 중 class attribute가 없는 td -> time (아침, 점심, 저녁)
+                        it.findElements(By.cssSelector("td:not([class])")).map {
+                            time -> timeBuf = time.text?: ""
+                            if (time.text != "") dormFoodMap[timeBuf] = ""
+                        }
+
+                        // tr 자식 중 class가 no_right left_pd인 td -> menu
+                        it.findElements(By.cssSelector("td.no_right.left_pd")).map {
+                            menu -> if (menu.text != "") dormFoodMap[timeBuf] = menu.text?: ""
+                        }
+                    }
+
+            dormFoodMap.forEach { (k, v) ->
+                println("key: $k")
+                println("value:")
+                println(v)
+            }
+        }
+
+        fun faculty() {
+
         }
     }
 
     object VacancyOf {
+        fun c1(): String? {
+            return ""
+        }
 
+        fun d1(): String? {
+            return ""
+        }
     }
 }
