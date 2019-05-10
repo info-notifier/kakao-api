c73b20420b4d090ae918a72c247e082027a32ed8
makehoney <makehoney3@gmail.com>
2019-05-11 08:54:04 +0900

    [Edit controller] 컨트롤러 로직 수정 due to change of crawler

--- src/main/kotlin/com/arimi/kakaoapi/contorollers/Controller.kt
+++ src/main/kotlin/com/arimi/kakaoapi/contorollers/Controller.kt
@@ -1,49 +1,18 @@
-package com.arimi.kakaoapi
+package com.arimi.kakaoapi.contorollers
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import java.sql.Timestamp
-
-data class Photo (
-        val url: String,
-        val width: Int,
-        val height: Int
-)
-data class MessageButton (
-        val label: String,
-        val url: String
-)
-data class Keyboard (
-        val type: String,
-        val buttons: List<String>
-)
-data class Message (
-        val text: String,
-        val photo: Photo,
-        val message_button: MessageButton
-)
-data class Response (
-        val message: Message,
-        val keyboard: Keyboard
-)
+import com.arimi.kakaoapi.*
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class Controller {
-    @GetMapping("/crawl")
-    fun page(@RequestParam(value = "url", defaultValue = "http://www.ajou.ac.kr/main/life/food.jsp") url: String): Response {
-//        Crawler.FoodCourt.of("dorm")
-//        val foo = Crawler.Vacancy.of("D1")
-        val timestamp = Timestamp(System.currentTimeMillis());
-
-        val msgBtn = MessageButton("상세정보", "http://u-campus.ajou.ac.kr/ltms/temp/261.png")
-        val photo = Photo("http://u-campus.ajou.ac.kr/ltms/temp/261.png?t=${timestamp.time}", 720, 630)
-        val vacancy = Crawler.Vacancy.of("D1")
-
-        val message = Message(vacancy, photo, msgBtn)
-        val keyboard = Keyboard("buttons", listOf("C1 열람실", "처음으로"))
-
-        return Response(message, keyboard)
+    @PostMapping("/message")
+    fun message(@RequestBody body: RequestMessageVO): ResponseVO {
+        return when (body.content) {
+            "C1 열람실" -> Crawler.Vacancy.of("C1")
+            "D1 열람실" -> Crawler.Vacancy.of("D1")
+            "C1 열람실 플러그 위치" -> Crawler.Vacancy.of("C1")
+            else -> Crawler.Vacancy.of("C1")
+        }
     }
 
     @GetMapping("/test")
