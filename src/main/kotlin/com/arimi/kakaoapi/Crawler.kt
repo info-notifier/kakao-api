d51ecf6083decaace5b475fd278f3bc3ce332aea
makehoney <makehoney3@gmail.com>
2019-05-11 08:53:26 +0900

    [Update crawler] 크롤러에서 대부분 로직 처리하도록 수정 (사용단-controller-의 편리성 도모)

--- src/main/kotlin/com/arimi/kakaoapi/Crawler.kt
+++ src/main/kotlin/com/arimi/kakaoapi/Crawler.kt
@@ -5,12 +5,18 @@ import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import java.sql.Timestamp
 
 // 싱글톤 패턴 적용
 class Crawler {
     companion object {
         private val options = ChromeOptions().addArguments("headless")
         private val driver = ChromeDriver(options)
+
+        lateinit var message: MessageVO
+        lateinit var keyboard: KeyboardVO
+        lateinit var msgBtn: MessageButtonVO
+        lateinit var photo: PhotoVO
     }
 
     // Using Selenium
@@ -62,19 +68,54 @@ class Crawler {
 
     // Using JSoup
     object Vacancy {
-        fun of(type: String): String {
-            val url = "http://u-campus.ajou.ac.kr/ltms/rmstatus/vew.rmstatus?bd_code=JL&rm_code=JL0$type"
+        private data class MetaData(val url: String, val buttons: List<String>)
+        private val getMetaDataFor: HashMap<String, MetaData> = hashMapOf (
+                "C1" to MetaData (
+                        "http://u-campus.ajou.ac.kr/ltms/temp/241.png",
+                        listOf("D1 열람실", "C1 열람실", "C1 열람실 플러그 위치", "처음으로")
+                ),
+                "D1" to MetaData (
+                        "http://u-campus.ajou.ac.kr/ltms/temp/261.png",
+                        listOf("C1 열람실", "D1 열람실", "처음으로")
+                )
+        )
+
+        private fun makeTextFor(place: String): String {
+            val url = "http://u-campus.ajou.ac.kr/ltms/rmstatus/vew.rmstatus?bd_code=JL&rm_code=JL0$place"
             var textList: List<String>
             var retText: String
             Jsoup.connect(url).get().run {
                 textList = select("td[valign=\"middle\"]")[1]
                         .allElements.eachText()[0].split(" ")
                         .map { it.trim() }
-                retText = "◆ $type 열람실의 이용 현황\n" +
+                retText = "◆ $place 열람실의 이용 현황\n" +
                         "  * 남은 자리: ${textList[6]}\n" +
                         "  * 사용률: ${textList[8].toInt() - textList[6].toInt()} / ${textList[8]} (${textList[12]})"
             }
             return retText
+        }
+
+        fun of(place: String): ResponseVO {
+            val url = getMetaDataFor[place]?.url
+            val buttons = getMetaDataFor[place]?.buttons
+            val timestamp = Timestamp(System.currentTimeMillis())
+            val text = makeTextFor(place)
+
+            url?.let {
+                photo = PhotoVO("$url?=${timestamp.time}", 720, 630)
+                msgBtn = MessageButtonVO("상세정보", it)
+                message = MessageVO(text, photo, msgBtn)
+            }
+            buttons?.let { keyboard = KeyboardVO("buttons", buttons) }
+
+            // url, buttons가 null로 들어오는 경우 exception 처리 어떻게할까 뭐 그럴리는 없지만..
+            // F1 열람실 같은 경우 C1처럼나오네
+            return try {
+                ResponseVO(message, keyboard)
+            } catch (e: UninitializedPropertyAccessException) {
+                println("here")
+                throw e
+            }
         }
     }
 }
