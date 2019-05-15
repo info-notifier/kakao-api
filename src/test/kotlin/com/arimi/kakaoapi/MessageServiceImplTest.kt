e869ebac512a5deb40f07c8da2fce933a84393d4
makehoney <makehoney3@gmail.com>
2019-05-15 14:58:57 +0900

    [Add test] vacancy service 기초 테스트 작성

--- src/test/kotlin/com/arimi/kakaoapi/MessageServiceImplTest.kt
+++ src/test/kotlin/com/arimi/kakaoapi/MessageServiceImplTest.kt
@@ -3,36 +3,35 @@ package com.arimi.kakaoapi
 import com.arimi.kakaoapi.repository.VacancyMessageRepository
 import com.arimi.kakaoapi.service.MessageServiceImpl
 import com.arimi.kakaoapi.vo.*
-import org.junit.jupiter.api.BeforeAll
+import com.arimi.kakaoapi.dao.VacancyDAO
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.*
-import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(MockitoExtension::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
-class MessageServiceImplTest2 {
+class MessageServiceImplTest {
     @Mock
     lateinit var repo: VacancyMessageRepository
 
     @InjectMocks
     lateinit var service: MessageServiceImpl
 
-
-    @BeforeAll
-    fun buildVO() {
-//        val imgUrl = repo.getMetaDataFor()
-    }
-
+    @Nested
     inner class VacancyTest {
+        @Test
         fun c1 () {
-            val imgUrl = repo.getMetaDataFor["C1"]?.imgUrl
-            val buttons = repo.getMetaDataFor["C1"]?.buttons
+            val imgUrl = VacancyDAO.getMetaDataFor["C1"]?.imgUrl
+            val buttons = VacancyDAO.getMetaDataFor["C1"]?.buttons
             val photo = PhotoVO(imgUrl!!, 720, 630)
             val msgBtn = MessageButtonVO("상세정보", imgUrl)
             val message = MessageVO("crawled data", photo, msgBtn)
@@ -42,15 +41,37 @@ class MessageServiceImplTest2 {
 
 
             // given
-            given(repo.getMessage("C1"))
-                    .willReturn(res)
+            given(repo.getMessage("C1")).willReturn(res)
 
             // when
-            val a = service.getMessage("C1 열람실")
+            val response = service.getMessage("C1 열람실")
 
             // then
             verify(repo, atLeastOnce()).getMessage(anyString())
-            
+            assertEquals(res.message.photo.height, response.message.photo.height)
+        }
+
+        @Test
+        fun d1 () {
+            val imgUrl = VacancyDAO.getMetaDataFor["D1"]?.imgUrl
+            val buttons = VacancyDAO.getMetaDataFor["D1"]?.buttons
+            val photo = PhotoVO(imgUrl!!, 720, 630)
+            val msgBtn = MessageButtonVO("상세정보", imgUrl)
+            val message = MessageVO("crawled data", photo, msgBtn)
+            val keyboard = KeyboardVO("buttons", buttons!!)
+
+            val res = ResponseVO(message, keyboard)
+
+
+            // given
+            given(repo.getMessage("D1")).willReturn(res)
+
+            // when
+            val response = service.getMessage("D1 열람실")
+
+            // then
+            verify(repo, atLeastOnce()).getMessage(anyString())
+            assertEquals(res.message.photo.height, response.message.photo.height)
         }
     }
 }
