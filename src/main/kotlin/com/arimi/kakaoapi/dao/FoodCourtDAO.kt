4f4e8631dc573608a5dc4bdac9eca77ea73e1fef
makehoney <makehoney3@gmail.com>
2019-08-24 16:22:28 +0900

    [Add DAO] 식당 기능 관련 DAO 추가

--- src/main/kotlin/com/arimi/kakaoapi/dao/FoodCourtDAO.kt
+++ src/main/kotlin/com/arimi/kakaoapi/dao/FoodCourtDAO.kt
@@ -1,2 +1,25 @@
 package com.arimi.kakaoapi.dao
 
+class FoodCourtDAO {
+    companion object {
+        data class MetaData(val type: String, val imgUrl: String?, val buttons: List<String>)
+
+        val getMetaDataFor: HashMap<String, MetaData> = hashMapOf (
+                "student" to MetaData (
+                        "buttons",
+                        null,
+                        listOf("기숙사식당", "교직원식당", "처음으로")
+                ),
+                "dormitory" to MetaData (
+                        "buttons",
+                        null,
+                        listOf("학생식당", "교직원식당", "처음으로")
+                ),
+                "faculty" to MetaData (
+                        "buttons",
+                        null,
+                        listOf("학생식당", "기숙사식당", "처음으로")
+                )
+        )
+    }
+}
