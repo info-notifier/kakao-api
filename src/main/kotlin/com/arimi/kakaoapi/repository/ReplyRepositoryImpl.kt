f156b51014b0b9a1a3c93456d75723c97e183534
makehoney <makehoney3@gmail.com>
2019-08-24 16:19:52 +0900

    [Refactor Repository] DAO 변경에 따른 코드 리팩토링 및 findFoodCourtReply 메도스 추가

--- src/main/kotlin/com/arimi/kakaoapi/repository/ReplyRepositoryImpl.kt
+++ src/main/kotlin/com/arimi/kakaoapi/repository/ReplyRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.arimi.kakaoapi.repository
 
+import com.arimi.kakaoapi.dao.FoodCourtDAO
 import com.arimi.kakaoapi.utils.Crawler
 import com.arimi.kakaoapi.vo.*
 import com.arimi.kakaoapi.dao.VacancyDAO
@@ -15,17 +16,16 @@ class ReplyRepositoryImpl : ReplyRepository {
 
     override fun findVacancyReply(place: String): ReplyVO {
         val text = Crawler.Vacancy.getTextOf(place)
-        val imgUrl = VacancyDAO.getMetaDataFor[place]?.imgUrl
-        val buttons = VacancyDAO.getMetaDataFor[place]?.buttons
+        val metadata = VacancyDAO.getMetaDataFor[place]
         val timestamp = Timestamp(System.currentTimeMillis())
-        imgUrl?.let {
-            photo = PhotoVO("$it?=${timestamp.time}", 720, 630)
-            msgBtn = MessageButtonVO("상세정보", it)
-            message = MessageVO(text, photo, msgBtn)
-        }
-        buttons?.let { keyboard = KeyboardVO("buttons", it) }
-        // TODO: imgUrl, buttons가 null로 들어오는 경우 exception 처리
 
+        metadata?.let {
+            photo = PhotoVO("${it.imgUrl!!}?t=${timestamp.time}", 720, 630)
+            msgBtn = MessageButtonVO("상세정보", it.imgUrl)
+            message = MessageVO(text, photo, msgBtn)
+            keyboard = KeyboardVO(it.type, it.buttons)
+        }
+        // TODO: imgUrl, buttons가 null로 들어오는 경우 exception 처리
         return try {
             ReplyVO(message, keyboard)
         } catch (e: UninitializedPropertyAccessException) {
@@ -34,23 +34,29 @@ class ReplyRepositoryImpl : ReplyRepository {
     }
 
     override fun findPlugReply(): ReplyVO {
+        val metadata = VacancyDAO.getMetaDataFor["plug"]
         val text = " * 플러그의 위치에 병아리가 있어요.\n" +
                 "왼쪽 병아리부터 플러그와 가까운 자리 번호입니다.\n" +
                 "(하단의 이미지는 실시간 이미지가 아닙니다.)\n" +
                 "349, 380, 405 or 412, 444, 468, 473"
-        val imgUrl = VacancyDAO.getMetaDataFor["plug"]?.imgUrl
-        val buttons = VacancyDAO.getMetaDataFor["plug"]?.buttons
 
-        imgUrl?.let {
-            photo = PhotoVO(it, 720, 200)
+        metadata?.let {
+            photo = PhotoVO(it.imgUrl!!, 720, 200)
             message = MessageVO(text, photo)
+            keyboard = KeyboardVO(it.type, it.buttons)
         }
-        buttons?.let { keyboard = KeyboardVO("buttons", buttons) }
 
         return ReplyVO(message, keyboard)
     }
 
     override fun findFoodCourtReply(place: String): ReplyVO {
+        val getText = Crawler.FoodCourt.getFnOf[place]
+        val metadata = FoodCourtDAO.getMetaDataFor[place]
+
+        metadata?.let {
+            message = MessageVO(getText!!.invoke())
+            keyboard = KeyboardVO(it.type, it.buttons)
+        }
         return ReplyVO(message, keyboard)
     }
 }
