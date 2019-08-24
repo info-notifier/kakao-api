278b11c36549a0cecfeefae245159f4e87b4bbf5
makehoney <makehoney3@gmail.com>
2019-08-24 16:24:18 +0900

    [Update Util] 크롤러 학생식당 관련 크롤링 기능 추가

--- src/main/kotlin/com/arimi/kakaoapi/utils/Crawler.kt
+++ src/main/kotlin/com/arimi/kakaoapi/utils/Crawler.kt
@@ -9,7 +9,7 @@ class Crawler {
             var textList: List<String>
             var retText: String
             Jsoup.connect(url).get().run {
-                textList = select("td[valign=\"middle\"]")[1]
+                textList = this.select("td[valign=\"middle\"]")[1]
                         .allElements.eachText()[0].split(" ")
                         .map { it.trim() }
                 retText = "◆ $place 열람실의 이용 현황\n" +
@@ -17,6 +17,46 @@ class Crawler {
                         "  * 사용률: ${textList[8].toInt() - textList[6].toInt()} / ${textList[8]} (${textList[12]})"
             }
             return retText
+        }
+    }
+
+    object FoodCourt {
+        val getFnOf: HashMap<String, () -> String> = hashMapOf (
+                "student" to this::getStudentText,
+                "dormitory" to this::getDormitoryText,
+                "faculty" to this::getFacultyText
+        )
+
+        private fun getStudentText(): String {
+            val url = "http://www.ajou.ac.kr/main/life/food.jsp"
+            var retText = ""
+            var hasMenu = false
+
+            Jsoup.connect(url).get().run {
+                this.select("table.ajou_table")[0].select("tr").forEach {tr ->
+                    tr.children().forEach { el ->
+                        // Build Menu header
+                        if (el.hasText() && !el.hasClass("no_right left_pd") && el.tagName() != "th") {
+                            val menuHeader = el.text()
+                            retText += "[${menuHeader.trim()}]\n"
+                        // Build Menu
+                        } else {
+                            val menu = el.children().select("li").eachText().joinToString("\n")
+                            if (menu.trim() != "") hasMenu = true
+                            retText += "${menu.trim()}\n\n"
+                        }
+                    }
+                }
+            }
+            return if (hasMenu) retText.trim() else ""
+        }
+
+        private fun getDormitoryText(): String {
+            return ""
+        }
+
+        private fun getFacultyText(): String {
+            return ""
         }
     }
 }
