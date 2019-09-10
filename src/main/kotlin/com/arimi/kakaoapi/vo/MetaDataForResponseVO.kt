cd7d6e5a85578a1f92da0b4f0b9bfe5ae6803a18
makehoney <makehoney3@gmail.com>
2019-09-10 22:25:44 +0900

    [Edit VO] MetaDataForResponseVO 기본 생성자 추가되도록 기본값 설정

--- src/main/kotlin/com/arimi/kakaoapi/vo/MetaDataForResponseVO.kt
+++ src/main/kotlin/com/arimi/kakaoapi/vo/MetaDataForResponseVO.kt
@@ -1,9 +1,11 @@
 package com.arimi.kakaoapi.vo
 
+// 생성자에 val type: String = "" 와 같이 default 값을 지정해줘야
+// default constructor가 생성되고 jackson을 이용한 JSON(to Object) deserialization이 가능하다.
 data class MetaDataForResponseVO(
-        val type: String,
-        val imgUrl: String?,
-        val buttons: List<String>
+        val type: String = "",
+        val imgUrl: String? = null,
+        val buttons: List<String> = listOf()
 ) {
     override fun equals(other: Any?): Boolean {
         return if (other == null || other !is MetaDataForResponseVO)
