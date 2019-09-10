0672622be7f88f4726467b8cb552c69469f74686
makehoney <makehoney3@gmail.com>
2019-09-10 18:44:26 +0900

    [Add VO] 응답 메타데이터 VO 추가

--- src/main/kotlin/com/arimi/kakaoapi/vo/MetaDataForResponseVO.kt
+++ src/main/kotlin/com/arimi/kakaoapi/vo/MetaDataForResponseVO.kt
@@ -1,2 +1,14 @@
 package com.arimi.kakaoapi.vo
 
+data class MetaDataForResponseVO(
+        val type: String,
+        val imgUrl: String?,
+        val buttons: List<String>
+) {
+    override fun equals(other: Any?): Boolean {
+        return if (other == null || other !is MetaDataForResponseVO)
+            false
+        else
+            other.type == type && other.imgUrl == imgUrl && other.buttons == buttons
+    }
+}
