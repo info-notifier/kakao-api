40472c8cc329ddcbf24f9b7e5d29eb1ad33463c5
makehoney <makehoney3@gmail.com>
2019-09-29 03:00:27 +0900

    [Update Util] 크롤러 스케쥴링 기능 추가

--- src/main/kotlin/com/arimi/kakaoapi/utils/Crawler.kt
+++ src/main/kotlin/com/arimi/kakaoapi/utils/Crawler.kt
@@ -3,10 +3,33 @@ package com.arimi.kakaoapi.utils
 import com.arimi.kakaoapi.libs.scrapers.FoodCourtScraper
 import com.arimi.kakaoapi.libs.scrapers.VacancyScraper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
 class Crawler @Autowired constructor (
+        redisTemplate: StringRedisTemplate,
         val vacancy: VacancyScraper,
         val foodCourt: FoodCourtScraper
-)
+) {
+    private val ops = redisTemplate.opsForValue()
+    private val vacancyPlaces = listOf("C1", "D1")
+    private val foodCourtPlaces = listOf("student", "dormitory", "faculty")
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000 * 60)
+    fun setVacancy() {
+        vacancyPlaces.forEach{place ->
+            val text = vacancy.getTextOf(place)
+            ops.set(place, text)
+        }
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000 * 60 * 60)
+    fun setFoodCourt() {
+        foodCourtPlaces.forEach{place ->
+            val text = foodCourt.getTextOf(place)
+            ops.set(place, text)
+        }
+    }
+}
