c06969a1899adb5febd92856dbbe62b741e6ec9b
makehoney <makehoney3@gmail.com>
2019-09-29 02:58:55 +0900

    [Add DAO] redis 캐시에 접근하는 dao 추가

--- src/main/kotlin/com/arimi/kakaoapi/dao/ScrappedTextDAO.kt
+++ src/main/kotlin/com/arimi/kakaoapi/dao/ScrappedTextDAO.kt
@@ -1,2 +1,16 @@
 package com.arimi.kakaoapi.dao
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class ScrappedTextDAO @Autowired constructor(
+        redisTemplate: StringRedisTemplate
+) {
+    private val ops = redisTemplate.opsForValue()
+
+    // scraper 라이브러리는 어떤 경우에도 null을 반환하지 않기 때문에 !! 연산을 쓴다.
+    // FoodCourtScraper의 경우 scrap할 텍스트가 없는 경우에는 빈 스트링을 반환하도록 구현하였음
+    fun getText(place: String): String = ops.get(place)!!
+}
