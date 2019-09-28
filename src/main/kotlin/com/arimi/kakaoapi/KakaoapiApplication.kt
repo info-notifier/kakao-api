39099e76f9fb9610389cbc0ecbe6fe7538defaab
makehoney <makehoney3@gmail.com>
2019-09-29 02:57:22 +0900

    [Add annotation] 스케쥴링 작업을 위해 애너테이션 추가

--- src/main/kotlin/com/arimi/kakaoapi/KakaoapiApplication.kt
+++ src/main/kotlin/com/arimi/kakaoapi/KakaoapiApplication.kt
@@ -2,10 +2,12 @@ package com.arimi.kakaoapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 // @SpringBootApplication 어노테이션은
 // @Configuration, @EnableAutoConfiguration 그리고 @ComponentScan을 디폴트 속성으로 함께 사용하는 것과 같다.
 @SpringBootApplication
+@EnableScheduling
 class KakaoapiApplication
 
 fun main(args: Array<String>) {
