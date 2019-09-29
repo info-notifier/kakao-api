b252a31ec07cc73628d4d14692c3b8f8f074cffb
makehoney <makehoney3@gmail.com>
2019-09-29 21:45:32 +0900

    [Edit Util] 사용 DAO 변경

--- src/main/kotlin/com/arimi/kakaoapi/utils/ButtonManager.kt
+++ src/main/kotlin/com/arimi/kakaoapi/utils/ButtonManager.kt
@@ -1,14 +1,14 @@
 package com.arimi.kakaoapi.utils
 
 import com.arimi.kakaoapi.dao.MenuNameMapperDAO
-import com.arimi.kakaoapi.dao.ScrappedTextDAO
+import com.arimi.kakaoapi.dao.RedisDAO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class ButtonManager @Autowired constructor(
         private val mapperDAO: MenuNameMapperDAO,
-        private val scrappedTextDAO: ScrappedTextDAO
+        private val redisDAO: RedisDAO
 ) {
     // 해당 식당에 내용이 있으면 filteredButtons에 버튼을 추가하고 내용이 없으면 버튼을 추가핳지 않는다.
     fun getFoodCourtFilteredButton(foodCourtButtons: List<String>): List<String> {
@@ -16,7 +16,7 @@ class ButtonManager @Autowired constructor(
 
         foodCourtButtons.forEach { button ->
             val menu = mapperDAO.getMenuName(button)
-            if(scrappedTextDAO.getText(menu) != "") {
+            if(redisDAO.getScrappedText(menu) != "") {
                 filteredButtons.add(button)
             }
         }
