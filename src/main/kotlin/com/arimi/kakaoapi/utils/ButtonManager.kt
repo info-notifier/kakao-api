0c8d2528dcb8052923bdc6595204c0cb8671be78
makehoney <makehoney3@gmail.com>
2019-09-29 19:09:22 +0900

    [Add Util] 버튼을 식당 상황에 따라서 동적으로 생성해주는 유틸리티 추가

--- src/main/kotlin/com/arimi/kakaoapi/utils/ButtonManager.kt
+++ src/main/kotlin/com/arimi/kakaoapi/utils/ButtonManager.kt
@@ -1,4 +1,27 @@
 package com.arimi.kakaoapi.utils
 
-class ButtonCreator {
+import com.arimi.kakaoapi.dao.MenuNameMapperDAO
+import com.arimi.kakaoapi.dao.ScrappedTextDAO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ButtonManager @Autowired constructor(
+        private val mapperDAO: MenuNameMapperDAO,
+        private val scrappedTextDAO: ScrappedTextDAO
+) {
+    // 해당 식당에 내용이 있으면 filteredButtons에 버튼을 추가하고 내용이 없으면 버튼을 추가핳지 않는다.
+    fun getFoodCourtFilteredButton(foodCourtButtons: List<String>): List<String> {
+        val filteredButtons = mutableListOf<String>()
+
+        foodCourtButtons.forEach { button ->
+            val menu = mapperDAO.getMenuName(button)
+            if(scrappedTextDAO.getText(menu) != "") {
+                filteredButtons.add(button)
+            }
+        }
+
+        filteredButtons.add("처음으로")
+        return filteredButtons
+    }
 }
