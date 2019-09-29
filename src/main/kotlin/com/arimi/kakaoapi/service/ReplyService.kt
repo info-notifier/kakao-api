bb73c9b70e36fab95df51c6dfb34411a21e8da69
leeby me <leeby.me@kakaocorp.com>
2019-09-29 16:21:44 +0900

    [Update Service] keyboard init 관련 서비스 로직 추가

--- src/main/kotlin/com/arimi/kakaoapi/service/ReplyService.kt
+++ src/main/kotlin/com/arimi/kakaoapi/service/ReplyService.kt
@@ -1,7 +1,9 @@
 package com.arimi.kakaoapi.service
 
+import com.arimi.kakaoapi.vo.KeyboardVO
 import com.arimi.kakaoapi.vo.ReplyVO
 
 interface ReplyService {
+    fun getKeyboardInitReply(): KeyboardVO
     fun getReplyByContent(content: String): ReplyVO
 }
--- src/main/kotlin/com/arimi/kakaoapi/service/ReplyServiceImpl.kt
+++ src/main/kotlin/com/arimi/kakaoapi/service/ReplyServiceImpl.kt
@@ -2,6 +2,7 @@ package com.arimi.kakaoapi.service
 
 import com.arimi.kakaoapi.exception.WrongRequestException
 import com.arimi.kakaoapi.repository.ReplyRepository
+import com.arimi.kakaoapi.vo.KeyboardVO
 import com.arimi.kakaoapi.vo.ReplyVO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Service
 class ReplyServiceImpl @Autowired constructor (
         private val replyRepository: ReplyRepository
 ) : ReplyService {
+    // 서비스는 여러개의 레포를 가져와서 사용하는 상위 레이어인데 네이밍이 reply로 replyrepository와 같은게 좀 이상함
+
+    override fun getKeyboardInitReply(): KeyboardVO = replyRepository.findKeyboardInitReply()
+
     override fun getReplyByContent(content: String): ReplyVO {
         return when (content) {
             // TODO: 처음으로 버튼 분기 필요
