3e2e4a6925ec49c68459593dac109b1a23714876
leeby me <leeby.me@kakaocorp.com>
2019-09-29 16:17:29 +0900

    [Update Controller] keyboard init 컨트롤러 추가

--- src/main/kotlin/com/arimi/kakaoapi/contoroller/Controller.kt
+++ src/main/kotlin/com/arimi/kakaoapi/contoroller/Controller.kt
@@ -1,6 +1,7 @@
 package com.arimi.kakaoapi.contoroller
 
 import com.arimi.kakaoapi.service.ReplyService
+import com.arimi.kakaoapi.vo.KeyboardVO
 import com.arimi.kakaoapi.vo.RequestMessageVO
 import com.arimi.kakaoapi.vo.ReplyVO
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +12,16 @@ import java.lang.RuntimeException
 class Controller @Autowired constructor (
         private val replyService: ReplyService
 ) {
+    @GetMapping("/keyboard")
+    fun keyboard(): KeyboardVO {
+        return try {
+            replyService.getKeyboardInitReply()
+        } catch (e: RuntimeException) {
+            System.err.println(e)
+            throw e
+        }
+    }
+
     @PostMapping("/message")
     fun message(@RequestBody body: RequestMessageVO): ReplyVO? {
         return try {
