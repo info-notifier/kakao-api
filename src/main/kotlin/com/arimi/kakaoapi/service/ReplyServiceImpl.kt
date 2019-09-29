e838234e7f6c81510444cbde01a8bc13974da212
makehoney <makehoney3@gmail.com>
2019-09-29 16:30:34 +0900

    [Git test] git 계정 테스트

--- src/main/kotlin/com/arimi/kakaoapi/service/ReplyServiceImpl.kt
+++ src/main/kotlin/com/arimi/kakaoapi/service/ReplyServiceImpl.kt
@@ -12,7 +12,7 @@ class ReplyServiceImpl @Autowired constructor (
         private val replyRepository: ReplyRepository
 ) : ReplyService {
     // 서비스는 여러개의 레포를 가져와서 사용하는 상위 레이어인데 네이밍이 reply로 replyrepository와 같은게 좀 이상함
-
+    // git test
     override fun getKeyboardInitReply(): KeyboardVO = replyRepository.findKeyboardInitReply()
 
     override fun getReplyByContent(content: String): ReplyVO {
